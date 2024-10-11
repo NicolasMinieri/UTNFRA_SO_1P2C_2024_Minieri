d2bc143c322bf98067ada3ebada3279460bce46f
holaprtf <nicolasminieri141@gmail.com>
2024-10-11 18:39:18 +0000

    feat: correccion de grupos en usuarios despeus de crearlos, punto c terminado

--- RTA_SCRIPT_Examen_20241008/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_C.sh
@@ -11,14 +11,23 @@ sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$HASH" p1c2_2024_A2
 sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$HASH" p1c2_2024_A3 
 sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$HASH" p1c2_2024_P1 
 
-# chown -R "el parametro -R es para que sea recursivo" propietario nuevo : path del archivo
+# chown -R "el parametro -R es para que sea recursivo" propietario nuevo :grupo : path del archivo
 
-sudo chown -R p1c2_2024_A1 /Examenes-UTN/alumno_1
-sudo chown -R p1c2_2024_A2 /Examenes-UTN/alumno_2
-sudo chown -R p1c2_2024_A3 /Examenes-UTN/alumno_3
-sudo chown -R p1c2_2024_P1 /Examenes-UTN/profesores
+sudo chown -R p1c2_2024_A1:p1c2_2024_gAlumno /Examenes-UTN/alumno_1
+sudo chown -R p1c2_2024_A2:p1c2_2024_gAlumno /Examenes-UTN/alumno_2
+sudo chown -R p1c2_2024_A3:p1c2_2024_gAlumno /Examenes-UTN/alumno_3
+sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+
+#modificando permisos recursivamente con chmod
 
 sudo chmod -R 750 /Examenes-UTN/alumno_1
 sudo chmod -R 760 /Examenes-UTN/alumno_2
 sudo chmod -R 700 /Examenes-UTN/alumno_3
 sudo chmod -R 773 /Examenes-UTN/profesores
+
+#accediendo al usuario y ejecutando el comando whoami y que la salida se guarde en un txt
+
+sudo su -c "whoami > /Examenes-UTN/alumno_1/validar.txt" p1c2_2024_A1
+sudo su -c "whoami > /Examenes-UTN/alumno_2/validar.txt" p1c2_2024_A2
+sudo su -c "whoami > /Examenes-UTN/alumno_3/validar.txt" p1c2_2024_A3
+sudo su -c "whoami > /Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
