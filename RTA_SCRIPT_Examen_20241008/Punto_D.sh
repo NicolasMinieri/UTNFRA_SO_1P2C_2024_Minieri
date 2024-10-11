bc91cae7b3ca39f28eadfdf1e411b16fe94a0a79
holaprtf <nicolasminieri141@gmail.com>
2024-10-11 19:47:31 +0000

    feat: Estructura asimetrica script terminado, punto_d

---
+++ RTA_SCRIPT_Examen_20241008/Punto_D.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+mkdir -p $HOME/Estructura_Asimetrica/{{correo,cliente}/cartas_{1..100},correo/cartero_{1..10}}
+
+
+tree $HOME/Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
