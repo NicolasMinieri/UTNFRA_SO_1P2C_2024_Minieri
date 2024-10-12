183ff94a5af6dc14cc9b124077f59a17131c62b4
holaprtf <nicolasminieri141@gmail.com>
2024-10-12 22:07:41 +0000

    feat: se agrego que cree un archivo de filtro avanzado con toda la info en el punto_F

--- RTA_SCRIPT_Examen_20241008/Punto_F.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_F.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+touch ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+
+echo "" > ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
 
 IPPUBLICA=$(curl -s ifconfig.me)
 
@@ -9,14 +12,16 @@ HASH=$(sudo grep $USUARIO /etc/shadow | awk -F ':' '{print $2}')
 
 URLREPO=$(git remote get-url origin)
 
-echo ""
-echo "Mi ip publica es: $IPPUBLICA"
-echo ""
-echo "Mi usuario es: $USUARIO"
-echo ""
-echo "Mi hash de mi usuario es: $HASH"
-echo ""
-echo "Mi ip publica es $IPPUBLICA"
-echo ""
-echo "La url de mi repositorio es: $URLREPO"
-echo ""
+touch ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+
+echo "" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "Mi ip publica es: $IPPUBLICA" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "Mi usuario es: $USUARIO" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "Mi hash de mi usuario es: $HASH" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "Mi ip publica es $IPPUBLICA" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "La url de mi repositorio es: $URLREPO" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
+echo "" >> ../RTA_ARCHIVOS_Examen_20241008/filtro_avanzado.txt
